4263f37a5118958352090a271b5d98ffbe85d625
Bruno Monteiro <bruno84@gmail.com>
2022-03-18 16:26:17 -0300

    melhorias

--- 1_Containers/ex1_1_vector.cpp
+++ 1_Containers/ex1_1_vector.cpp
@@ -5,13 +5,29 @@
 using namespace std; 
  
 /*
-Vectors são contêineres sequenciais que representam arrays que podem mudar de tamanho.
-Assim como os arrays, os vectors usam armazenamento contíguos na memória para seus elementos, 
-com a mesma eficiência dos arrays. 
-Mas, diferentemente dos arrays, seu tamanho pode mudar dinamicamente.
-Portanto, em comparação com os arrays, os vectors consomem mais memória 
+Vectors sao conteineres sequenciais que representam arrays que podem mudar de tamanho.
+Assim como os arrays, os vectors usam armazenamento contiguos na memoria para seus elementos, 
+com a mesma eficiencia dos arrays. 
+Porem, diferentemente dos arrays, seu tamanho pode mudar dinamicamente.
+Portanto, em comparacao com os arrays, os vectors consomem mais memoria, 
 em troca da capacidade de gerenciar o armazenamento e crescer dinamicamente de forma eficiente.
 */
+
+
+
+void show(vector<int>::iterator begin, vector<int>::iterator end) 
+{ 
+	cout << endl; 
+	
+	// auto : keyword usado em contextos que o compilador ja subentende qual sera o tipo.
+	// 		  Deve ser usado com moderacao.
+    for (auto it = begin; it != end; it++) 
+	{
+		cout << *it << " ";
+	}
+        
+	cout << endl<<endl;
+}
 
 void show(int array[], int size) 
 { 
@@ -19,7 +35,7 @@ void show(int array[], int size)
     for(int i = 0; i < size; ++i) {
     	cout << array[i] << " ";	
 	}
-	cout << endl;
+	cout << endl<<endl;
 }
   
 void show(vector<int> v) 
@@ -29,58 +45,101 @@ void show(vector<int> v)
     for(int i = 0; i < size; ++i) {
     	cout << v[i] << " ";	
 	}
-	cout << endl; 
+	cout << endl<<endl;
 }
   
 int main() 
 {
-	// ARRAY
-    int array[] = {1, 1, 7, 1, 1, 8 }; 	
-    int n = sizeof(array) / sizeof(array[0]); 
-    cout << "ARRAY: "; 
-	show(array, n);		
+	cout << "ARRAY: "; 
+    int array[] = {10, 20, 20, 20, 30, 30, 20, 20, 10}; 	
     
-    // ARRAY -> VECTOR
-    vector<int> vetor(array, array+n);
-    cout << endl << "VECTOR: "; 
-	show(vetor);
+	int sizeArray = sizeof(array) / sizeof(array[0]); 
+
+	show(array, sizeArray);		
+    cout << "sizeArray: " << sizeArray << endl<<endl; 
+    
+    
+    cout << "VECTOR: " << endl; 
+    
+	// Declaracao sem informar o tamanho e depois adiciono elementos no final
+    vector<int> vetor1;	 
+    
+    vetor1.push_back(10);
+    vetor1.push_back(20);
+    vetor1.push_back(30);
+    
+    cout << "vetor1: "; 
+    show(vetor1);
+    
+	// Declaracao informando o tamanho e depois preencho todos com um valor padrao
+	vector<int> vetor2( 5 );	
+	 
+    fill(vetor2.begin(), vetor2.end(), -1);
+    
+    cout << "vetor2: "; 
+    show(vetor2);
+    
+    // Inicializacao preenchendo todos com um valor padrao 
+    vector<int> vetor3(10, -1);	 // (size, valorInicial)
+	cout << "vetor3: "; 
+    show(vetor3);
+
+    // Inicializacao com base em um conjunto
+    vector<int> vetor4 = { 10, 20, 30 };
+    cout << "vetor4: "; 
+    show(vetor4);
+
+    
+	// Inicializacao com base em um array
+    vector<int> vetor5(array, array+sizeArray);		// 10, 20, 20, 20, 30, 30, 20, 20, 10 	
+    cout << "vetor5: "; 
+	show(vetor5);
+
+	cout << "COUNT: ";
+	// Conta quantos elementos o valor aparece no vector.
+    cout << count(vetor5.begin(), vetor5.end(), 20) << endl<<endl;  // 5
+
+	cout << "UNIQUE: " << endl;
+	// Retorna um iterator que aponta para o ultimo elemento apos o ultimo elemento valido.
+	vector<int>::iterator last = unique( vetor5.begin(), vetor5.end() );  // 10 20 30 20 10 (30) (20) (20) (10)
+	cout << "last = " << *last;			// 30  pois o ultimo elemento valido eh o 10 								 *
+	show(vetor5);	
+	
+	cout << "DISTANCE: " << endl;
+	// Retorna a quantidade de elementos entre o inicio e o fim.
+	int dist = distance( vetor5.begin(), last );
+	cout << "dist = " << dist << endl;					// 5  (quantidade de elementos uteis)
+	cout << "size = " << vetor5.size() << endl<<endl;	// 9
+	
+	cout << "RESIZE: " << endl;
+	// Altera o tamanho do vector
+	vetor5.resize( dist ); 					// 10 20 30 20 10
+	cout << "size = " << vetor5.size();		// 5
+	show(vetor5);
+	
+	cout << "BEGIN e END: ";
+	// OBS: end() nao retorna o iterator do ultimo elemento,
+	// 		mas sim o iterator apos o ultimo (past-the-end).
+	show( vetor5.begin(), vetor5.end() );  // 0, 5
 			
-	cout << endl <<"UNIQUE: " << endl;
-	vector<int>::iterator last = unique( vetor.begin(), vetor.end() );  // : 1, 7, 1, 8, (1), (8)
-	cout << "last = " << *last << endl;			// 1 (new end)
-	
-	cout << endl <<"DISTANCE: " << endl;
-	int dist = distance(vetor.begin(), last );
-	cout << "dist = " << dist << endl;			// 4
-	
-	cout << endl << "SIZE: " << endl;
-	n = vetor.size();
-	cout << "n = " << n << endl;				// 6
-	
-	cout << endl << "ERASE: (intervalo)";
-    vetor.erase(last, vetor.end() ); 			// 1, 7, 1, 8	
-    show(vetor); 
-  
-	cout << endl << "SIZE: " << endl;
-	n = vetor.size();
-	cout << "n = " << n;						// 4
-  	show(vetor); 
-  	
-	cout << endl << "COUNT: ";
-    cout << count(vetor.begin(), vetor.end(), 1) << endl; // 2 = 1 aparece duas vezes
-  
-    cout << endl << "FIND: "; 
-    vector<int>::iterator pos = find(vetor.begin(), vetor.end(), 8); 
-    cout << pos - vetor.begin() << endl; 		// 3
+	cout << "ERASE (intervalo): " << endl;
+	// Remove um trecho do vector. Parametros: inicio, fim (past-the-end)
+    vetor5.erase(vetor5.begin(), vetor5.end() - 3 );  // 0, (5-3=2)
+	cout << "size = " << vetor5.size();		// 3	
+    show(vetor5); 			// Removeu o inicio: 10, 20 	Resultou: 30 20 10
+	  
+    cout << "FIND: " << endl; 
+    // Retorna o iterator para a primeira ocorrencia do argumento.
+    vector<int>::iterator pos = find(vetor5.begin(), vetor5.end(), 10); 
+    cout << "index de pos: " << pos - vetor5.begin() << endl; 	// 2
       
   	cout << endl << "ERASE: (elemento) " << endl;
-	int indexToDelete = 2;
-    vetor.erase(vetor.begin() + indexToDelete); 
+	int indexToDelete = 1;			// quero apagar o 20
+    vetor5.erase(vetor5.begin() + indexToDelete); 
 		
-	cout << endl << "SIZE: " << endl;
-	n = vetor.size();
-	cout << "n = " << n;						// 3
-  	show(vetor); 
+	cout << "size = " << vetor5.size();	 // 2
+  	show(vetor5); 
+  	
 
     return 0; 
 } 
